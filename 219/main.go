5a3631dea4360f01c29e5d549cce1ceb3a20d22a
Baquit <arthur.baquit@hotmail.com>
2022-10-21 17:23:07 +0900

    small improv

--- 219/main.go
+++ 219/main.go
@@ -1,28 +1,20 @@
 package main
 
 func containsNearbyDuplicate(nums []int, k int) bool {
-	visited := make(map[int][]int)
+	visited := make(map[int]int)
 	for i, num := range nums {
 		if _, ok := visited[num]; !ok {
-			visited[num] = []int{i}
+			visited[num] = i
 			continue
 		}
-		for _, v := range visited[num] {
-			if absInt(v, i) <= k {
-				return true
-			}
+		if i-visited[num] <= k {
+			return true
 		}
-		visited[num] = append(visited[num], i)
+		visited[num] = i
 	}
 	return false
 }
 
-func absInt(x, y int) int {
-	if x-y < 0 {
-		return y - x
-	}
-	return x - y
-}
 func main() {
 	println(containsNearbyDuplicate([]int{1, 2, 3, 1}, 3))
 	println(containsNearbyDuplicate([]int{1, 0, 1, 1}, 1))
