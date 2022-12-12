e8a42bbe459799db02c7961b1b9440dc5e1cd8f0
Baquit <arthur.baquit@hotmail.com>
2022-12-12 16:00:32 +0900

    add: 278 go solution

---
+++ solution-by-topics/binary-tree/278/main.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+func isBadVersion(version int) bool {
+	return version >= 4
+}
+
+func firstBadVersion(n int) int {
+	left, right := 1, n
+	for left < right {
+		mid := left + (right-left)/2
+		if isBadVersion(mid) {
+			right = mid
+			continue
+		}
+		left = mid + 1
+	}
+	return right
+}
+
+func main() {
+	println(firstBadVersion(5))
+	println(firstBadVersion(1))
+}
