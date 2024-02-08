52b7c448e2441769b3038121d7865ae84101f4de
Baquit <arthur.baquit@hotmail.com>
2024-02-08 18:00:42 -0300

    add: 279 go solution

---
+++ random-problems/279/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "math"
+
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		dp[i] = math.MaxInt32
+	}
+	dp[0] = 0
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+
+	return dp[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	println(numSquares(12))
+	println(numSquares(13))
+}
