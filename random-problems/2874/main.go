091c1b0c75ec8e91cea84c132fba4c1c6b06bfad
Baquit <arthur.baquit@hotmail.com>
2025-04-03 08:15:24 -0300

    add: 2874 alt golang solution

--- random-problems/2874/main.go
+++ random-problems/2874/main.go
@@ -14,8 +14,22 @@ func maximumTripletValue(nums []int) int64 {
 	return ans
 }
 
+func maximumTripletValueOnePass(nums []int) int64 {
+	var ans int64
+	maxDiff, maxI := 0, 0
+	for _, num := range nums {
+		ans = max(ans, int64(maxDiff)*int64(num))
+		maxDiff = max(maxDiff, maxI-num)
+		maxI = max(maxI, num)
+	}
+	return ans
+}
+
 func main() {
-	println(maximumTripletValue([]int{12, 6, 1, 2, 7}))  // 77
-	println(maximumTripletValue([]int{1, 10, 3, 4, 19})) // 133
-	println(maximumTripletValue([]int{1, 2, 3}))         // 0
+	println(maximumTripletValue([]int{12, 6, 1, 2, 7}))         // 77
+	println(maximumTripletValue([]int{1, 10, 3, 4, 19}))        // 133
+	println(maximumTripletValue([]int{1, 2, 3}))                // 0
+	println(maximumTripletValueOnePass([]int{12, 6, 1, 2, 7}))  // 77
+	println(maximumTripletValueOnePass([]int{1, 10, 3, 4, 19})) // 133
+	println(maximumTripletValueOnePass([]int{1, 2, 3}))         // 0
 }
