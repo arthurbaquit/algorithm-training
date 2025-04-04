a853e9b5e4c4f0ba3d6f71237c5c261d767aa023
Baquit <arthur.baquit@hotmail.com>
2025-04-04 09:50:52 -0300

    add: 865 golang solution

---
+++ random-problems/865/main.go
@@ -0,0 +1,32 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func subtreeWithAllDeepest(root *TreeNode) *TreeNode {
+	ans, _ := dfs(root, 0)
+	return ans
+}
+
+func dfs(root *TreeNode, currDepth int) (*TreeNode, int) {
+	if root == nil {
+		return nil, currDepth
+	}
+	maximumL, leftD := dfs(root.Left, currDepth+1)
+	maximumR, rightD := dfs(root.Right, currDepth+1)
+	if leftD == rightD {
+		return root, leftD
+	}
+	if leftD > rightD {
+		return maximumL, leftD
+	}
+	return maximumR, rightD
+}
+
+func main() {
+	root := &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 5}}, Right: &TreeNode{Val: 3, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 7}}}
+	_ = subtreeWithAllDeepest(root)
+}
