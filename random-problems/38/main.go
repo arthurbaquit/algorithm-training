61c5e17451d01a3743dca6420d7dbe9633e095ee
Baquit <arthur.baquit@hotmail.com>
2025-04-18 06:42:46 -0300

    add: 38 iterative golang solution

--- random-problems/38/main.go
+++ random-problems/38/main.go
@@ -22,9 +22,40 @@ func countAndSay(n int) string {
 	return res
 }
 
+func countAndSayIterative(n int) string {
+	if n == 1 {
+		return "1"
+	}
+	if n == 2 {
+		return "11"
+	}
+	ans := ""
+	curr := "11"
+	for i := 2; i < n; i++ {
+		slow, fast, count := 0, 1, 1
+		for slow < len(curr) {
+			for fast < len(curr) && curr[fast] == curr[slow] {
+				fast++
+				count++
+			}
+			countToStr := strconv.Itoa(count)
+			ans += countToStr + string(curr[slow])
+			slow = fast
+			fast = slow + 1
+			count = 1
+		}
+		curr = ans
+		ans = ""
+	}
+	return curr
+}
+
 func main() {
-	println(countAndSay(5))
-	println(countAndSay(4))
-	println(countAndSay(1))
+	println(countAndSay(5))          //  "111221"
+	println(countAndSay(4))          // "1211"
+	println(countAndSay(1))          // "1"
+	println(countAndSayIterative(5)) // "111221"
+	println(countAndSayIterative(4)) // "1211"
+	println(countAndSayIterative(1)) // "1"
 
 }
