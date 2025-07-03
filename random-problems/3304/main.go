37cf2a77ec2f1acb66a2908a8c84e48f7eee1454
Baquit <arthur.baquit@hotmail.com>
2025-07-03 08:33:01 -0300

    add: 3304 golang editorial solution

--- random-problems/3304/main.go
+++ random-problems/3304/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/bits"
+
 var word []byte
 
 func kthCharacter(k int) byte {
@@ -21,7 +23,22 @@ func kthCharacter(k int) byte {
 	return word[k-1]
 }
 
+func kthCharacterEditorial(k int) byte {
+	ans := 0
+	for k != 1 {
+		t := bits.Len(uint(k)) - 1
+		if 1<<t == k {
+			t--
+		}
+		k -= 1 << t
+		ans++
+	}
+	return byte('a' + ans)
+}
+
 func main() {
-	println(string(kthCharacter(5)))  // b
-	println(string(kthCharacter(10))) // c
+	println(string(kthCharacter(5)))           // b
+	println(string(kthCharacter(10)))          // c
+	println(string(kthCharacterEditorial(5)))  // b
+	println(string(kthCharacterEditorial(10))) // c
 }
